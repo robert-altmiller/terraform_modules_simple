35a22107ee0eacf85e9333cfe5e4b901ba91bdf0
robert altmiller <robert.altmiller@databricks.com>
2024-04-29 10:57:36 -0500

    initial upload

--- databricks_dev/aws_mws/aws_create_iam_role.tf
+++ databricks_dev/aws_mws/aws_create_iam_role.tf
@@ -1,6 +1,11 @@
 # AWS IAM role creation
 resource "aws_iam_role" "cross_account_role" {
   name               = local.vars.aws.iam.role_name
+  #assume_role_policy = data.aws_iam_policy_document.workspace_role_assume_role.json
+  #inline_policy {
+  #  name   = "databricks_workspace_policy"
+  #  policy = data.aws_iam_policy_document.workspace_role_inline_policy.json
+  #}
   assume_role_policy = data.databricks_aws_assume_role_policy.this.json
   tags               = local.vars.aws.iam.role_tags
 }
